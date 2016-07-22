ff82625902e373c329aed22f5526cb9f462373fd
iavanish <avanish1404@iiitd.ac.in>
2016-07-22 10:18:37 +0530

    Donegit add . The bakchod bot works :)

--- Bakcho/fb_Bakcho/views.py
+++ Bakcho/fb_Bakcho/views.py
@@ -4,11 +4,12 @@ from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
-import pprint
 import requests
+import MySQLdb
 
 # Create your views here.
 class BakchoView(generic.View):
+	
 	def get(self, request, *args, **kwargs):
 		if self.request.GET['hub.verify_token'] == '5432167890':
 			return HttpResponse(self.request.GET['hub.challenge'])
@@ -21,10 +22,41 @@ class BakchoView(generic.View):
 
 	def post(self, request, *args, **kwargs):
 		incoming_message = json.loads(self.request.body.decode('utf-8'))
+		counts = dict()
+		max = 0
+		maxTextID = 0
+		db = MySQLdb.connect("localhost", "root", "root", "hackday")
+		cursor = db.cursor()
 		for entry in incoming_message['entry']:
 			for message in entry['messaging']:
 				if 'message' in message:
-					post_facebook_message(message['sender']['id'], message['message']['text'])
+					tokens = message['message']['text'].split()
+					for t in tokens:
+						sql = "select textID from invertedIndex where keyword = '%s'" % t
+						try:
+							cursor.execute(sql)
+							results = cursor.fetchall()
+							for row in results:
+								textID = row[0]
+								print "textID %d" % textID
+								counts[textID] = counts.get(textID, 0) + 1
+								if(max < counts.get(textID, 0)):
+									max = counts.get(textID, 0)
+									maxTextID = textID
+						except:
+							print "Error1: unable to fetch data"
+
+					sql = "select text from textDescription where textID = '%d'" % maxTextID
+					try:
+						cursor.execute(sql)
+						results = cursor.fetchall()
+						for row in results:
+							text1 = row[0]
+					except:
+						print "Error: unable to fetch data"
+					db.close()
+					print text1
+					post_facebook_message(message['sender']['id'], text1)
 		return HttpResponse()
 
 
